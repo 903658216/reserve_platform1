52536b9c23a213257d6f5667ad70c6b8df2a5822
jerome <903658216@qq.com>
2020-06-29 17:43:31 +0800

    完成了酒店客房和酒店客房价格的增删查改功能，进行了统一配置V1.2

---
+++ micro_service/micro_elasticsearch/src/main/java/com/jh/micro_elasticsearch/MicroElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.jh.micro_elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroElasticsearchApplication.class, args);
+    }
+
+}
